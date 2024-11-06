2f465f1e50871de417136675e5d7cf35b9161bea
Victormarks10 <aaron.victormarks10@gmail.com>
2024-11-06 16:18:11 -0300

    feat(produto): adicionar lista de produtos com descrições

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
-
-const searchForm = document.querySelector('.search-form')
+const searchForm = document.querySelector('.search-form');
 const buttonSearch = document.querySelector('.button-search');
+const productList = document.querySelector('.product-list');
 
-searchForm.addEventListener('submit',  async function(event) {
+searchForm.addEventListener('submit', async function (event) {
     event.preventDefault();
-    const imputValue = event.target[0].value
+    const inputValue = event.target[0].value;
 
-    const data = await fetch (`https://api.mercadolibre.com/sites/MLB/search?q=iphone${imputValue}`)
+    const data = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=iphone${inputValue}`);
 
-    const Products = (await data.json()).results.slice(0,10)
+    const products = (await data.json()).results.slice(0, 10);
 
-    console.log(Products)
-})
+    displayData(products);
+});
 
-
-
-
+function displayData(products) {
+    console.log(products);
+    productList.innerHTML = products.map(product =>
+        `
+        <div class='product-card'>   
+            <img src="${product.thumbnail}" alt="Imagem do produto">
+            <p>${product.title}</p>
+            <p>Preço: R$ ${product.price}</p>
+        </div>
+        `
+    ).join('');
+}
